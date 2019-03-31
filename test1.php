faf6a45fad9f688e5a0d2e4aeaa29d0dcc289cea
TAREK YOUSSEF ELWAN <tareqelwan@gmail.com>
2019-03-31 08:17:41 +0300

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,20 @@
+<html>
+<title>
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.css"/>
+
+
+</title>
+
+<body>
+
+</body>
+
+  <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js"></script>
+
+</html>
+
+<?php
+
+
+
+<?>
